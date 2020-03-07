5e8836a24c1dcb49a916299f8e007a1ec84fe5f3
ochoaangel <44645810+ochoaangel@users.noreply.github.com>
2020-03-07 13:20:30 -0400

    * en Menú agregado icono de persona a Login

--- src/app/components/pop-menu/pop-menu.component.ts
+++ src/app/components/pop-menu/pop-menu.component.ts
@@ -14,7 +14,7 @@ export class PopMenuComponent implements OnInit {
     { titulo: 'Home', url: '/home', icon: 'pin' },
     { titulo: 'Comprar ticket', url: '/buy-your-ticket', icon: 'md-bus' },
     { titulo: 'Panel de Usuario', url: '/user-panel', icon: 'apps' },
-    { titulo: 'Login', url: '/login', icon: 'finger-print' },
+    { titulo: 'Login', url: '/login', icon: 'person' },
     { titulo: 'Contacto', url: '/contact', icon: 'md-call' },
     { titulo: 'Términos y condiciones', url: '/terms-conditions', icon: 'md-information-circle' },
   ]
